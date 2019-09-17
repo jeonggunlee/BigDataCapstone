23277cee27d2f405238cd87983c3d6bccda59503
Jeong-Gun Lee <jeonggun.lee@gmail.com>
2019-09-17 16:37:22 +0900

    Update source1.c

--- Source/source1.c
+++ Source/source1.c
@@ -1,3 +1,9 @@
-# Project main source code
-# 프로젝트 메인 소스 코드!!!!
+// Project main source code
+// 프로젝트 메인 소스 코드!!!!
 
+#include <stdio.h>
+main()
+{
+  poo();  // BBB가 만들어줘
+  foo();  // CCC가 만들어줘!!!
+}
